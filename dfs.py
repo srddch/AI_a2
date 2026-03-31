47238a5273f037f643d88560afbdd7fd2c5ade71
Zhenghao Sun <105965706@student.eud.au>
2026-03-31 16:13:31 +1100

    第2版

--- dfs.py
+++ dfs.py
@@ -11,20 +11,20 @@ def search(graph, coords, start, goals):
 
     while stack:
         current = stack.pop()
-        nodes_expanded += 1
-
-        if current.state in goals:
-            return current.state, nodes_expanded, reconstruct_path(current)
 
         if current.state in visited:
             continue
 
         visited.add(current.state)
 
+        nodes_expanded += 1
+
+        if current.state in goals:
+            return current.state, nodes_expanded, reconstruct_path(current)
+
         neighbors = sorted(graph[current.state], key=lambda x: x[0], reverse=True)
 
         for neighbor, cost in neighbors:
-            if neighbor not in visited:
-                stack.append(Node(neighbor, current))
+            stack.append(Node(neighbor, current))
 
     return None, nodes_expanded, []
